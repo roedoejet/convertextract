141c2f91da7c40dcb4f9e2f5203dc482d3530140
Fineen Davis <fdavi093@uottawa.ca>
2020-07-27 15:39:45 -0400

    added main

--- convertextract/parsers/eaf_parser.py
+++ convertextract/parsers/eaf_parser.py
@@ -1,7 +1,8 @@
-from .utils import BaseParser
+import pympi
+from convertextract.parsers.utils import BaseParser
 
 class Parser(BaseParser):
-     """Extract text from ELAN file using pympi-ling.
+    """Extract text from ELAN file using pympi-ling.
     """
 
     def extract(self, filename, **kwargs):
@@ -13,32 +14,23 @@ class Parser(BaseParser):
         converted_filename = filename[:-4] + '_converted.eaf'
         
         # Here is where you should parse and convert the Elan file
-        elan_data = pympi.Elan.parse_eaf(filename, eaf_obj???)
-        for tier in elan_data:
-            tiers = self.get_tier_names() #returned as a list
+        eaf_obj = pympi.Elan.Eaf()
+        elan_data = pympi.Elan.parse_eaf(filename, eaf_obj)
+        tiers = elan_data.get_tier_names() #returned as a list
+        for tier in tiers:
+            if tier == "aligned_annotations" or "reference_annotations":
+                result = elan_data.get_annotation_data_for_tier(tier) #Gives a list of annotations of the form: (begin, end, value)
+                annotation = result[2]
 
+        transducer(annotation).output_string
 
-get_annotation_data_for_tier(id_tier)
-#Gives a list of annotations of the form: (begin, end, value) When the tier contains reference annotations this will be returned, check get_ref_annotation_data_for_tier() for the format.
-#Parameters:	id_tier (str) – Name of the tier.
-#Raises KeyError:
-# 	If the tier is non existent.
-
-
-pympi.Elan.parse_eaf(file_path, eaf_obj)
-# Parse an EAF file
-
-# Parameters:	
-# file_path (str) – Path to read from, - for stdin.
-# eaf_obj (pympi.Elan.Eaf) – Existing EAF object to put the data in.
-# Returns:	
-# EAF object.
-        
-
-
-        # To get output just do transducer(INPUT).output_string
         if "no_write" not in kwargs or not kwargs['no_write']:
             # This should save the file!!!
             pass
         # This should return the converted text
         return
+
+
+if __name__ == '__main__':
+    print('helloooo')
+    # put your stuff here
