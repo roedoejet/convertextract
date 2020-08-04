fef42ed75ef5db8e7ee308896de9fced32f8e2a8
Fineen Davis <fdavi093@uottawa.ca>
2020-08-04 13:38:51 -0400

    baby steps

--- convertextract/parsers/eaf_parser.py
+++ convertextract/parsers/eaf_parser.py
@@ -12,25 +12,30 @@ class Parser(BaseParser):
             transducer = self.get_transducer(kwargs.get('input_language', ''), 
                                              kwargs.get('output_language', ''))
             converted_filename = filename[:-4] + '_converted.eaf'
-            return converted_filename
         print("SUCCESS", converted_filename)
-        # # Here is where you should parse and convert the Elan file
-        # eaf_obj = pympi.Elan.Eaf()
-        # elan_data = pympi.Elan.parse_eaf(filename, eaf_obj)
-        # tiers = elan_data.get_tier_names() #returned as a list
-        # for tier in tiers:
-        #     if tier == "aligned_annotations" or "reference_annotations":
-        #         result = elan_data.get_annotation_data_for_tier(tier) #Gives a list of annotations of the form: (begin, end, value)
-        #         annotation = result[2]
+        # Here is where you should parse and convert the Elan file
+        eaf_obj = pympi.Elan.Eaf(filename)
+        tiers = eaf_obj.get_tier_names() #returned as a list
+        all_results = []
+        for tier in tiers:
+            result = eaf_obj.get_annotation_data_for_tier(tier) #Gives a list of annotations of the form: (begin, end, value)
+            #iterates over each list within the tiers
+            for res in result:
+                #change value in place to a string
+                #converts 'value' using g2p
+                #adds converted data to all_results
+                value_res = str(res[2])
+                new_res = transducer(value_res).output_string
+                all_results.append(new_res)
 
-        # transducer(annotation).output_string
+                value_res.remove_all_annotations_from_tier(tiers)
+                value_res.add_annotation(tiers, value = "new_res")
 
-        # if "no_write" in kwargs or kwargs['no_write']:
-        #     pass
-        # else:
-        #     elan_data.save(converted_filename)
-
-        # return transducer(annotation).output_string
+        if "no_write" in kwargs or kwargs['no_write']:
+            pass
+        else:
+            pympi.Elan.to_eaf(converted_filename, eaf_obj, pretty=True)
+        return ' '.join(all_results)
 
 
 #if __name__ == '__main__':
