2e82d0afe9a8f6d8004a662177c575d4fe314046
Fineen Davis <fdavi093@uottawa.ca>
2020-08-04 17:11:38 -0400

    parser now works

--- convertextract/parsers/eaf_parser.py
+++ convertextract/parsers/eaf_parser.py
@@ -15,26 +15,46 @@ class Parser(BaseParser):
         print("SUCCESS", converted_filename)
         # Here is where you should parse and convert the Elan file
         eaf_obj = pympi.Elan.Eaf(filename)
+        new_eaf_obj = pympi.Elan.Eaf()
         tiers = eaf_obj.get_tier_names() #returned as a list
         all_results = []
         for tier in tiers:
-            result = eaf_obj.get_annotation_data_for_tier(tier) #Gives a list of annotations of the form: (begin, end, value)
+            new_eaf_obj.add_tier(tier)
             #iterates over each list within the tiers
+            try:
+                result = eaf_obj.get_annotation_data_for_tier(tier) #Gives a list of annotationS of the form: (begin, end, value)
+            except KeyError:
+                print(f"tier named {tier} does not work")
+                continue
+            #eaf_obj.remove_all_annotations_from_tier(tier)
             for res in result:
-                #change value in place to a string
-                #converts 'value' using g2p
-                #adds converted data to all_results
                 value_res = str(res[2])
                 new_res = transducer(value_res).output_string
                 all_results.append(new_res)
+            
+                # eaf_obj.remove_annotation(tier,(res[0]+res[1])/2)
 
-                value_res.remove_all_annotations_from_tier(tiers)
-                value_res.add_annotation(tiers, value = "new_res")
+                new_eaf_obj.add_annotation(tier, res[0], res[1], value = new_res)
+            # new_res = transducer(value_res).output_string
+            # eaf_obj.add_annotation(tier, result[0], result[1], value = new_res)
+            # result = eaf_obj.get_annotation_data_for_tier(tier) 
+            # all_results.append(new_res)
+            #Gives a list of annotations of the form: (begin, end, value)
+            #iterates over each list within the tiers
 
-        if "no_write" in kwargs or kwargs['no_write']:
+                #change value in place to a string
+                #converts 'value' using g2p
+                #adds converted data to all_results
+                # value_res = str(res[2])
+                # new_res = transducer(value_res).output_string
+
+                # value_res.remove_all_annotations_from_tier(tiers)
+                # value_res.add_annotation(tiers, value = "new_res")
+        if "no_write" not in kwargs or kwargs['no_write']:
             pass
         else:
-            pympi.Elan.to_eaf(converted_filename, eaf_obj, pretty=True)
+            print(f"should write to {converted_filename}")
+            pympi.Elan.to_eaf(converted_filename, new_eaf_obj, pretty=True)
         return ' '.join(all_results)
 
 
