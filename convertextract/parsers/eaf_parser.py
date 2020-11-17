1b2feb643776eba6144fee812021103ffda7a37c
roedoejet <aidanpine@shaw.ca>
2020-11-17 12:57:05 -0800

    fix(eaf): fix parser to read original file

--- convertextract/parsers/eaf_parser.py
+++ convertextract/parsers/eaf_parser.py
@@ -16,21 +16,20 @@ class Parser(BaseParser):
 
         # Here is where you should parse and convert the Elan file
         eaf_obj = pympi.Elan.Eaf(filename)
-        new_eaf_obj = pympi.Elan.Eaf()
+        new_eaf_obj = pympi.Elan.Eaf(filename)
         tiers = eaf_obj.get_tier_names() #returned as a list
+        new_eaf_obj.remove_tiers(eaf_obj.get_tier_names())
         all_results = []
         for tier in tiers:
             new_eaf_obj.add_tier(tier)
             #iterates over each list within the tiers
             result = eaf_obj.get_annotation_data_for_tier(tier) #Gives a list of annotationS of the form: (begin, end, value)
-           
             for res in result:
                 value_res = str(res[2])
                 new_res = transducer(value_res).output_string
                 all_results.append(new_res)
 
                 new_eaf_obj.add_annotation(tier, res[0], res[1], value = new_res)
-           
         if "no_write" in kwargs and kwargs['no_write']:
             pass
         else:
